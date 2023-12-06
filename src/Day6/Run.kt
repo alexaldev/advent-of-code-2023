1e51d5fa81f46254a21434dbc28fb8f0ac63eb53
AlexAlf <alexallafi.dev@gmail.com>
2023-12-06 20:38:25 +0200

    Solves Day06

--- src/Day6/Run.kt
+++ src/Day6/Run.kt
@@ -3,34 +3,58 @@ package Day6
 import QuizTaskExec
 import runPartQuizAndMeasureTime
 
-class FirstPart : QuizTaskExec {
+class FirstPart(
+    private val durationsToRecords: List<Pair<Int, Int>>
+) : QuizTaskExec {
+
     override fun exec(input: List<String>): Int {
-        TODO("Not yet implemented")
+
+        return durationsToRecords.map { durAndRec ->
+
+            val (raceDuration, record) = durAndRec
+
+            (0..raceDuration)
+                .map { it * (raceDuration - it) }
+                .count { it > record }
+
+        }.fold(1) { acc: Int, i: Int -> acc * i }
     }
 }
 
-class SecondPart : QuizTaskExec {
+class SecondPart(
+    private val durationToRecord: Pair<Long, Long>
+) : QuizTaskExec {
     override fun exec(input: List<String>): Int {
-        TODO("Not yet implemented")
+
+        val (raceDuration, record) = durationToRecord
+
+        return (0..raceDuration)
+            .asSequence()
+            .map { it * (raceDuration - it) }
+            .count { it > record }
     }
 }
 
 fun main() {
 
-    val firstPartTestExpected: Int = 100
+    val firstPartTestExpected: Int = 288
 
-    val firstPart: QuizTaskExec = FirstPart()
-    val secondPart: QuizTaskExec = SecondPart()
+    val firstPartTestInp = listOf(7 to 9, 15 to 40, 30 to 200)
+
+    val firstPart: QuizTaskExec = FirstPart(firstPartTestInp)
+
     val thisDayId = firstPart.javaClass.packageName.substringAfter("Day")
 
     val p1Duration = runPartQuizAndMeasureTime(firstPart, firstPartTestExpected, thisDayId)
 
     println("First part duration: ${p1Duration.inWholeMilliseconds}")
 
-    val secondPartTestExpected: Int = TODO()
-
     try {
 
+        val secondPartTestExpected: Int = 41382569
+        val secondPartInput = 62649190L to 553101014731074
+
+        val secondPart: QuizTaskExec = SecondPart(62649190L to 553101014731074)
         val p2Duration = runPartQuizAndMeasureTime(secondPart, secondPartTestExpected, thisDayId)
         println("Second part duration: ${p2Duration.inWholeMilliseconds}")
 
