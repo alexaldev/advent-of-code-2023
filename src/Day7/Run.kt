bc99e5b7863908ecc6e9cc12327a5415ebbabb44
AlexAlf <alexallafi.dev@gmail.com>
2023-12-11 20:41:04 +0200

    Solves Day07, Day08 part 1

--- src/Day7/Run.kt
+++ src/Day7/Run.kt
@@ -2,11 +2,117 @@ package Day7
 
 import QuizTaskExec
 import runPartQuizAndMeasureTime
+import java.lang.IllegalArgumentException
+import java.lang.IllegalStateException
+import java.util.Comparator
 
 class FirstPart : QuizTaskExec {
     override fun exec(input: List<String>): Int {
-        TODO("Not yet implemented")
+
+        val hands = input.map(::handFromLine)
+        val sorted = hands
+            .sortedWith(
+                Comparator.comparingInt<Hand> { it.type.strength }
+                .thenComparing { h -> h.cards }
+            )
+            .reversed()
+
+        return sorted
+            .map { it.bid }
+            .zip((sorted.size downTo 1))
+            .sumOf { it.first * it.second }
     }
+}
+
+class Cards(val values: String): Comparable<Cards> {
+
+    private val charsToStr = mapOf(
+        'A' to 15, 'K' to 14, 'Q' to 13, 'J' to 12, 'T' to 11, '9' to 9, '8' to 8, '7' to 7, '6' to 6, '5' to 5, '4' to 4, '3' to 3, '2' to 2
+    )
+    override fun compareTo(other: Cards): Int {
+
+
+        val firstDifferentChar = this.values.filterIndexed { index, c -> c != other.values[index] }.first()
+        val pos = this.values.indexOf(firstDifferentChar)
+        val otherChar = other.values[pos]
+
+        val myScore = charsToStr[firstDifferentChar]!!
+        val otherScore = charsToStr[otherChar]!!
+
+//        return if (myScore == otherScore) 0 else
+        val res = if (myScore >= otherScore) 1 else -1
+        println("Have to compare same Hand type: ${this.values} - ${other.values}, comparison output: $res")
+        return res
+    }
+}
+//249644885
+data class Hand(
+    val cards: Cards,
+    val type: HandType,
+    val bid: Int
+)
+
+fun handFromLine(line: String): Hand {
+
+    val (cards, bid) = line.split(" ")
+
+    return Hand(Cards(cards), extractType(cards), bid.toInt())
+}
+
+fun extractType(s: String): HandType {
+
+    fun checkForFiveOfAKind(s: String): Boolean {
+        return s.all { it == s.first() }
+    }
+
+    fun checkForFourOfAKind(s: String): Boolean {
+        val charsCounts = s.associateWith { s.count { c -> c == it } }
+        return charsCounts.size == 2 && charsCounts.containsValue(1) && charsCounts.containsValue(4)
+    }
+
+    fun checkForHighCard(s: String): Boolean {
+        return s.toCharArray().distinct().size == s.length
+    }
+
+    fun checkForFullHouse(s: String): Boolean {
+        val charsCounts = s.associateWith { s.count { c -> c == it } }
+        return charsCounts.size == 2 && charsCounts.containsValue(2) && charsCounts.containsValue(3)
+    }
+
+    fun checkForThreeOfAKind(s: String): Boolean {
+        val charsCounts = s.associateWith { s.count { c -> c == it } }
+        return charsCounts.size == 3 && charsCounts.containsValue(3) && charsCounts.containsValue(1) && charsCounts.containsValue(1)
+    }
+
+    fun checkForTwoPair(s: String): Boolean {
+        val charsCounts = s.associateWith { s.count { c -> c == it } }
+        return charsCounts.size == 3 && charsCounts.containsValue(2) && charsCounts.containsValue(2) && charsCounts.containsValue(1)
+    }
+
+    fun checkForOnePair(s: String): Boolean {
+        val charsCounts = s.associateWith { s.count { c -> c == it } }
+        return charsCounts.size == 4 && charsCounts.containsValue(2)
+    }
+
+    if (checkForFiveOfAKind(s)) return HandType.FiveOfAKind
+    if (checkForFourOfAKind(s)) return HandType.FourOfAKind
+    if (checkForFullHouse(s)) return HandType.FullHouse
+    if (checkForThreeOfAKind(s)) return HandType.ThreeOfAKind
+    if (checkForTwoPair(s)) return HandType.TwoPair
+    if (checkForOnePair(s)) return HandType.OnePair
+    if (checkForHighCard(s)) return HandType.HighCard
+
+    throw IllegalArgumentException("Could not find a matching type")
+}
+
+enum class HandType(val strength: Int) {
+    FiveOfAKind(7),
+    FourOfAKind(6),
+    FullHouse(5),
+    ThreeOfAKind(4),
+    TwoPair(3),
+    OnePair(2),
+    HighCard(1)
 }
 
 class SecondPart : QuizTaskExec {
@@ -17,7 +123,7 @@ class SecondPart : QuizTaskExec {
 
 fun main() {
 
-    val firstPartTestExpected: Int = 100
+    val firstPartTestExpected: Int = 6440
 
     val firstPart: QuizTaskExec = FirstPart()
     val secondPart: QuizTaskExec = SecondPart()
--- src/Day8/Run.kt
+++ src/Day8/Run.kt
@@ -2,34 +2,122 @@ package Day8
 
 import QuizTaskExec
 import runPartQuizAndMeasureTime
+import java.lang.IllegalStateException
 
 class FirstPart : QuizTaskExec {
     override fun exec(input: List<String>): Int {
-        TODO("Not yet implemented")
+
+        val instructions = input.first().toCharArray()
+        val nodesToDestinations = hashMapOf<String, Pair<String, String>>()
+
+        input.subList(2, input.size).map {
+            val spls = it.split("=")
+            val nodeStart = spls.first().trim()
+            val nodeDest = spls[1].trim().split(",")
+            val leftNodeDest = nodeDest.first().removePrefix("(").trim()
+            val rightNodeDest = nodeDest[1].removeSuffix(")").trim()
+
+            nodesToDestinations[nodeStart] = leftNodeDest to rightNodeDest
+        }
+
+        var count = 0
+        var currentNode = "AAA" // Start
+        var found = false
+        var currentInstructionIndex = 0
+        while(!found) {
+
+            val currentInstruction = instructions[currentInstructionIndex]
+
+            currentNode = when(currentInstruction) {
+                'R' -> {
+                    nodesToDestinations[currentNode]!!.second
+                }
+                'L' -> {
+                    nodesToDestinations[currentNode]!!.first
+                }
+                else -> throw IllegalStateException("Invalid instruction $currentInstruction")
+            }
+
+            count += 1
+
+            currentInstructionIndex = (currentInstructionIndex + 1) % instructions.size
+            if (currentNode == "ZZZ") {
+                found = true
+            }
+        }
+
+        return count
     }
 }
 
 class SecondPart : QuizTaskExec {
     override fun exec(input: List<String>): Int {
-        TODO("Not yet implemented")
+
+        val instructions = input.first().toCharArray()
+        val nodesToDestinations = hashMapOf<String, Pair<String, String>>()
+
+        input.subList(2, input.size).map {
+            val spls = it.split("=")
+            val nodeStart = spls.first().trim()
+            val nodeDest = spls[1].trim().split(",")
+            val leftNodeDest = nodeDest.first().removePrefix("(").trim()
+            val rightNodeDest = nodeDest[1].removeSuffix(")").trim()
+
+            nodesToDestinations[nodeStart] = leftNodeDest to rightNodeDest
+        }
+
+        val allStartingNodes = nodesToDestinations.filterKeys { it.endsWith("A") }
+
+        var count = 0
+        var allEndAtZ = false
+        var currentInstructionIndex = 0
+        val currentNodesKeys = mutableSetOf<String>().apply { addAll(allStartingNodes.keys) }
+
+        while(!allEndAtZ) {
+
+            val currentInstruction = instructions[currentInstructionIndex]
+
+            val toAdd = when(currentInstruction) {
+                'R' -> {
+                    currentNodesKeys.map { nodesToDestinations[it]!!.second }
+                }
+                'L' -> {
+                    currentNodesKeys.map { nodesToDestinations[it]!!.first }
+                }
+                else -> throw IllegalStateException("Invalid instruction $currentInstruction")
+            }
+
+            currentNodesKeys.clear()
+            currentNodesKeys.addAll(toAdd)
+
+            count += 1
+
+            currentInstructionIndex = (currentInstructionIndex + 1) % instructions.size
+            if (currentNodesKeys.all { it.endsWith("Z") }) {
+                allEndAtZ = true
+            }
+        }
+
+        return count
     }
 }
 
 fun main() {
 
-    val firstPartTestExpected: Int = 100
+    val firstPartTestExpected: Int = 6
 
     val firstPart: QuizTaskExec = FirstPart()
-    val secondPart: QuizTaskExec = SecondPart()
+
     val thisDayId = firstPart.javaClass.packageName.substringAfter("Day")
 
-    val p1Duration = runPartQuizAndMeasureTime(firstPart, firstPartTestExpected, thisDayId)
+//    val p1Duration = runPartQuizAndMeasureTime(firstPart, firstPartTestExpected, thisDayId)
 
-    println("First part duration: ${p1Duration.inWholeMilliseconds}")
-
-    val secondPartTestExpected: Int = TODO()
+//    println("First part duration: ${p1Duration.inWholeMilliseconds}")
 
     try {
+
+        val secondPart: QuizTaskExec = SecondPart()
+        val secondPartTestExpected: Int = 6
 
         val p2Duration = runPartQuizAndMeasureTime(secondPart, secondPartTestExpected, thisDayId)
         println("Second part duration: ${p2Duration.inWholeMilliseconds}")
